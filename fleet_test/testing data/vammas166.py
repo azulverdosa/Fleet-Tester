e2b2e9b9bc8956c64d517af3d3227ef596144def
Ava <atorre@tradewind.aero>
2023-11-14 17:22:23 -0500

    add new test data and  Poe created python logic

--- fleet_test/testing data/vammas166.py
+++ fleet_test/testing data/vammas166.py
@@ -1,24 +1,6 @@
-from vammas166 import reports
-import datetime
-
-# Sort the reports chronologically based on the "created_at" timestamp. This step ensures that the reports are in ascending order based on time.
-
-# Initialize variables to track the start and end timestamps for the "is_serviceable": false duration, as well as the total duration.
-
-# Iterate through the sorted reports and perform the following steps for each report:
-
-# Check if the "is_serviceable" value is false.
-# If it is false, set the start timestamp if it has not been set already.
-# If it is true and the start timestamp has been set, calculate the duration by subtracting the start timestamp from the current report's timestamp. Add this duration to the total duration.
-# Reset the start timestamp to None.
-# After iterating through all the reports, if the start timestamp is not None (indicating that the final report had "is_serviceable": false), calculate the duration from the start timestamp to the end of the time period (or the current time). Add this duration to the total duration.
-
-# The total duration represents the duration for which the vehicle had "is_serviceable": false.
-
-
-data = {
-    "reports": [
-         {
+reports = {
+  "dtreports": [
+    {
       "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
       "vehicle_name": "Vammas 166",
       "created_at (strftime)": "2023-07-18 19:58:51",
@@ -334,89 +316,10 @@ data = {
       "created_at (strftime)": "2023-07-10 17:18:13",
       "is_serviceable": False
     }
-    ]
+  ],
+  "dtreportsCount": 90,
+  "query_dict1": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+  "query_dict2": "tester",
+  "query_dict3": None
+  
 }
-
-reports = data["reports"]
-reports.sort(key=lambda x: x["created_at (strftime)"])
-
-start_timestamp = None
-total_duration = datetime.timedelta()
-
-for report in reports:
-    if not report["is_serviceable"]:
-        if start_timestamp is None:
-            start_timestamp = datetime.datetime.strptime(report["created_at (strftime)"], "%Y-%m-%d %H:%M:%S")
-    elif start_timestamp is not None:
-        current_timestamp = datetime.datetime.strptime(report["created_at (strftime)"], "%Y-%m-%d %H:%M:%S")
-        duration = current_timestamp - start_timestamp
-        total_duration += duration
-        start_timestamp = None
-
-# Check if the last report had "is_serviceable": false
-if start_timestamp is not None:
-    current_timestamp = datetime.datetime.now()
-    duration = current_timestamp - start_timestamp
-    total_duration += duration
-
-print("Total duration with is_serviceable=False:", total_duration)
-
-
-# TODO return should be:
-# Total duration with is_serviceable=False: 9 days, 1:59:14
-# ```
-# This indicates that the vehicle had an "is_serviceable": false duration of 9 days, 1 hour, 59 minutes, and 14 seconds.
-
-# Poe translated logic below ----------------------------------------------------------------------
-
-# from report_data import data
-# from datetime import datetime
-
-# another_uuid = '2ffd6494-68ae-4b4b-8a54-d2b36181b44f'
-# other_uuid = '0cc18e59-ee6a-4b0b-974a-2998686af408'
-# bmw1_uuid = 'e844b8b3-f627-462b-9a1d-514a7b3f0831'
-# multihog_uuid = '3ddf5d06-1323-4924-a038-203344e7d4c9'
-
-# def get_milliseconds(date_str=None):
-#     date_str = date_str or datetime.now().isoformat()
-#     return int(datetime.fromisoformat(date_str).timestamp() * 1000)
-
-# # this filter is not the one to use in real life
-# # throwaway code: mocks missing database
-# filtered = list(filter(lambda x: x['vehicleUuid'] == bmw1_uuid, reversed(data["objects"])))
-# # end of throwaway
-
-# results = {
-#     'start': None,
-#     'totalHours': 0
-# }
-
-# for index, report in enumerate(filtered):
-#     is_last_report_in_range = index == len(filtered) - 1
-
-#     if results['start'] is not None and report['isRTOInspectionRequired']:
-#         print('fixxd!')
-#         print('ended :>>', report['createdAt'])
-
-#         downtime_milliseconds = get_milliseconds(report['createdAt']) - get_milliseconds(results['start'])
-#         downtime_hours = round(downtime_milliseconds / 1000 / 60 / 60, 2)
-
-#         print('previousDowntimeHours :>>', results['totalHours'])
-#         print('downtimeHours :>>', downtime_hours)
-
-#         results['totalHours'] += downtime_hours
-#         results['start'] = None
-
-#     elif report['isUnsafe']:
-#         if results['start'] is not None:
-#             print('still unsafe')
-#         else:
-#             print('it broke!')
-#             print('start :>>', report['createdAt'])
-#             results['start'] = report['createdAt']
-
-#     print('usable')
-#     if report['isRTOInspectionRequired']:
-#         print('rto required')
-
-# print('Total Hours :>>', results['totalHours'])
