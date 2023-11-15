6334300f3d1e286a0e8426831efa745babda2dbf
Ava <atorre@tradewind.aero>
2023-11-15 17:26:29 -0500

    add & edit data

---
+++ fleet_test/data/all_reports_all_data.py
@@ -0,0 +1,505 @@
+all_reports = {
+  "objects": [
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-18 19:58:51",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-05 17:07:32",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-11-11 16:56:16",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-05-28 14:46:19",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-14 19:06:46",
+            "is_serviceable": True
+        },
+        {
+            "hours": 1,
+            "kilometers": 12,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "70c56a2c-31ba-495b-a090-a8cbc60d1a24",
+            "vehicle_name": "Delorean 1984",
+            "created_at": "2023-01-11 15:37:04",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-06-29 19:38:29",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": 121,
+            "isUnsafe": True,
+            "isRecurrent": True,
+            "vehicle_uuid": "da0fe2b4-c676-4298-9f26-b69dc81a53e3",
+            "vehicle_name": "liu test Vehicle",
+            "created_at": "2023-01-12 20:53:51",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-18 20:05:01",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0ef1eb16-bc3e-49e3-85c8-b626199ec454",
+            "vehicle_name": "A6",
+            "created_at": "2023-04-10 19:52:38",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-13 20:37:53",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": 11,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0ef1eb16-bc3e-49e3-85c8-b626199ec454",
+            "vehicle_name": "A6",
+            "created_at": "2023-04-10 21:36:54",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-14 20:38:46",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-14 21:19:01",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-05-27 14:35:12",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": True,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-05-24 17:15:16",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-18 21:18:20",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "4e607f20-2a0a-400f-8fdd-77c7b558048a",
+            "vehicle_name": "Delorean 1989",
+            "created_at": "2019-11-12 17:20:18",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2020-04-09 18:29:37",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-06-29 19:25:26",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": True,
+            "vehicle_uuid": "da0fe2b4-c676-4298-9f26-b69dc81a53e3",
+            "vehicle_name": "liu test Vehicle",
+            "created_at": "2023-05-30 21:04:18",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "f7be3a00-544b-44cf-8ed7-9781ab9bf58b",
+            "vehicle_name": "another",
+            "created_at": "2023-05-16 17:42:12",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-10-02 17:06:09",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-19 13:09:01",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-13 21:33:53",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-05 17:01:34",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-05 13:27:00",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-19 19:24:37",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-18 19:48:18",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": True,
+            "isRecurrent": False,
+            "vehicle_uuid": "da0fe2b4-c676-4298-9f26-b69dc81a53e3",
+            "vehicle_name": "liu test Vehicle",
+            "created_at": "2022-06-09 15:49:01",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-11-11 18:12:36",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-17 21:46:16",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2022-02-01 20:03:53",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-19 19:24:17",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-10 17:18:42",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "9ed345e7-f09f-431e-8b29-56727d9368bb",
+            "vehicle_name": "Delorean 1987",
+            "created_at": "2023-04-06 20:42:53",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-19 18:24:57",
+            "is_serviceable": None
+        },
+        {
+            "hours": 10,
+            "kilometers": 100,
+            "isUnsafe": True,
+            "isRecurrent": False,
+            "vehicle_uuid": "94666230-12cf-42d8-977b-8dc3ffd7d5e4",
+            "vehicle_name": "Snow Mauler 92",
+            "created_at": "2022-08-24 15:53:38",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-05 19:23:12",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-05 19:04:09",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "da0fe2b4-c676-4298-9f26-b69dc81a53e3",
+            "vehicle_name": "liu test Vehicle",
+            "created_at": "2023-06-08 14:24:41",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": 4000,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0001a731-853d-471d-a4fe-5ef5e6b5c1ab",
+            "vehicle_name": "Vammas 157",
+            "created_at": "2019-12-03 21:06:23",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "da0fe2b4-c676-4298-9f26-b69dc81a53e3",
+            "vehicle_name": "liu test Vehicle",
+            "created_at": "2023-05-04 15:00:27",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2022-04-14 16:08:02",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
+            "vehicle_name": "A1",
+            "created_at": "2023-05-16 17:40:41",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": 3456,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-05-28 14:43:09",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2021-01-28 22:30:36",
+            "is_serviceable": None
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "0591a6fe-24f4-4392-8525-03f48a16fa90",
+            "vehicle_name": "Vammas 166",
+            "created_at": "2023-07-19 14:55:45",
+            "is_serviceable": True
+        },
+        {
+            "hours": None,
+            "kilometers": 345,
+            "isUnsafe": True,
+            "isRecurrent": True,
+            "vehicle_uuid": "1dc9f8da-9818-446d-ba37-9cca161d22d0",
+            "vehicle_name": "Left Blower 188",
+            "created_at": "2019-06-20 15:18:40",
+            "is_serviceable": False
+        },
+        {
+            "hours": None,
+            "kilometers": None,
+            "isUnsafe": False,
+            "isRecurrent": False,
+            "vehicle_uuid": "31d68403-5a76-45dd-8385-cd031ab30f53",
+            "vehicle_name": "Delorean 1985",
+            "created_at": "2019-11-13 21:46:20",
+            "is_serviceable": True
+        }
+    ],
+  "totalCount": 248
+}
---
+++ fleet_test/data/delorean1984.py
@@ -0,0 +1,11 @@
+reports = {
+  "dtreports":[
+        {
+            "vehicle_uuid": "e214e890-a0bb-4085-b4ed-2aba72609c95",
+            "vehicle_name": "Delorean 1986",
+            "created_at (strftime)": "2023-06-29 21:12:23",
+            "is_serviceable": None
+        }
+    ],
+
+}
