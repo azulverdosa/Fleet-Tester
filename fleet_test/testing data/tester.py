ee3b3a8c26cde5d0dc6fcd9a4f75ccb954bc86c4
Ava <atorre@tradewind.aero>
2023-11-15 17:27:59 -0500

    move files around

--- fleet_test/testing data/tester.py
+++
@@ -1,9 +0,0 @@
-from report_data import data
-
-filtered_data = [vehicle for vehicle in data["objects"] if vehicle["isServiceable"]]
-
-for vehicle in filtered_data:
-    print(vehicle["createdAt"])
-    print(vehicle["vehicleUuid"])
-    print(vehicle["isServiceable"])
-    print()
