42e367ed2777c4bd41290820bf1a6fe94e0a16e1
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-11-16 21:05:57 -0500

    complete filtering

--- fleet_test/data/a1.py
+++ fleet_test/data/a1.py
@@ -1,63 +1,63 @@
 reports = {
-  "dtreports":[
+    "objects":[
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-05-16 17:40:41",
+            "created_at": "2023-05-16 17:40:41",
             "is_serviceable": None
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-03-07 16:50:59",
+            "created_at": "2023-03-07 16:50:59",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2022-08-11 18:47:55",
+            "created_at": "2022-08-11 18:47:55",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-05-16 17:39:55",
+            "created_at": "2023-05-16 17:39:55",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-01-05 21:08:12",
+            "created_at": "2023-01-05 21:08:12",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-03-07 21:20:32",
+            "created_at": "2023-03-07 21:20:32",
             "is_serviceable": None
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-03-07 16:49:53",
+            "created_at": "2023-03-07 16:49:53",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-01-10 18:50:30",
+            "created_at": "2023-01-10 18:50:30",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-03-07 16:52:19",
+            "created_at": "2023-03-07 16:52:19",
             "is_serviceable": True
         },
         {
             "vehicle_uuid": "c3916926-e3d7-411f-8750-b518aa86ce0f",
             "vehicle_name": "A1",
-            "created_at (strftime)": "2023-01-18 18:25:23",
+            "created_at": "2023-01-18 18:25:23",
             "is_serviceable": None
         }
     ]
